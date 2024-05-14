1ae16d2d7bd64b06ce592f9cb61218c0adcde056
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-05-14 22:23:48 +0600

    Color changes

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -13,21 +13,21 @@ const Banner = () => {
       <div className="hero-content flex items-center justify-start text-neutral-content">
         <div>
           <h1 className="mb-5 text-2xl md:text-4xl lg:text-5xl font-bold">
-            Welcome to KMSS Associates!
+            Welcome to <span className="text-[#FE322E] dark:text-white">KMSS</span> <span className="text-[#2563EB] dark:text-white">Associates!</span>
           </h1>
           <p className="mb-5 w-full md:w-3/5 lg:w-2/5 text-sm md:text-base lg:text-lg">
           Trusted legal and professional consultants specializing in corporate law, tax, accounting, VAT, and investment services. Get expert guidance and personalized support. Choose us for comprehensive solutions.
           </p>
           <div className="flex gap-4">
             <Link to={"/more-about-us"}>
-              <button className="py-2 px-3 lg:px-4 text-xs md:text-base bg-blue-600 text-white rounded-sm">
+              <button className="py-2 px-3 lg:px-4 text-xs md:text-base bg-[#2563EB] dark:bg-slate-800 text-white rounded-sm">
                 My Company
               </button>
             </Link>
 
             <a href="#services">
               {" "}
-              <button className="py-2 px-3 lg:px-4 text-xs md:text-base bg-orange-700 hover:bg-slate-950 duration-300 text-white rounded-sm">
+              <button className="py-2 px-3 lg:px-4 text-xs md:text-base bg-orange-700 dark:bg-slate-900 hover:bg-slate-950 duration-300 text-white rounded-sm">
                 Get in Touch
               </button>
             </a>
