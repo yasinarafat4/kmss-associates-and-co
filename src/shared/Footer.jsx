43fb0e5f7cb296103e761525a87b8236d0bb1e49
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-05-22 23:17:47 +0600

    Services added in the footer

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -26,10 +26,12 @@ const Footer = () => {
         </aside>
         <nav>
           <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <a className="link link-hover"><Link to={"/licencing-service"}>Licencing</Link></a>
+          <a className="link link-hover"><Link to={"/entity-formation-service"}>Entity Formation</Link></a>
+          <a className="link link-hover"><Link to={"/accounting-service"}>Accounting</Link></a>
+          <a className="link link-hover"><Link to={"/tax-advisory-service"}>Tax Advisory</Link></a>
+          <a className="link link-hover"><Link to={"/audit-and-assurance-service"}>Audit & Assurance</Link></a>
+          <a className="link link-hover"><Link to={"/ipo-capital-market-service"}>IPO & Capital Market</Link></a>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
