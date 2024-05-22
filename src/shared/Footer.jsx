b817db2846df6e51bfe6b2b32b48afb2f1201260
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-05-22 23:09:19 +0600

    New Footer items added

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -11,19 +11,35 @@ const Footer = () => {
 
   return (
     <footer className="bg-slate-950 dark:bg-slate-950 text-white pt-8 px-4 text-base lg:text-lg">
-      <div className="lg:grid grid-cols-3 gap-6 pb-8  md:px-10 xl:px-3">
-        <div className="col-span-1 pb-3 lg:pb-0">
+      <footer className="footer pb-4">
+        <aside>
           <Link to="/">
             <img className="w-20 md:w-28 pb-2" src={logo} alt="footer logo" />
           </Link>
           <p>
-            Providing exceptional legal and professional services with integrity
-            and excellence. Tailored solutions to meet your unique needs.
-            Contact us for expert advice and personalized support.
+            {" "}
+            Providing exceptional legal and professional services
+            <br className="hidden lg:block" /> with integrity and excellence. Tailored solutions to meet
+            your <br className="hidden lg:block" /> unique needs. Contact us for expert advice and
+            personalized support.
           </p>
-        </div>
-        <div className="col-span-2 justify-center">
-          <div className="md:flex justify-start items-center gap-4">
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <div className="lg:flex justify-start items-center gap-4">
             <div class="flex items-center pt-3">
               <img src={emailImg} alt="email icon" class="w-6 h-auto mr-2" />
               <p>kmssassociates52@gmail.com</p>
@@ -37,8 +53,8 @@ const Footer = () => {
             <img src={locaionImg} alt="location icon" class="w-6 h-auto mr-2" />
             <p>19/1, BL-K, Eastern Housing, Mirpur-12, Dhaka-1216</p>
           </div>
-        </div>
-      </div>
+        </nav>
+      </footer>
       <hr />
       <div className="text-center md:flex items-center justify-between py-4 md:px-10 xl:px-3">
         <p class="mt-4 text-base lg:text-lg">
