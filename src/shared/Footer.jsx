9013610a8933132ad211410ebe8fc17eb7e1dbcd
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-05-22 22:52:55 +0600

    Logo added

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../src/assets/kmss_logo.png";
 import callImg from "../assets/call.png";
 import emailImg from "../assets/email.png";
 import locaionImg from "../assets/location.png";
@@ -11,44 +13,37 @@ const Footer = () => {
     <footer className="bg-slate-950 dark:bg-slate-950 text-white pt-8 px-4 text-base lg:text-lg">
       <div className="lg:grid grid-cols-3 gap-6 pb-8  md:px-10 xl:px-3">
         <div className="col-span-1 pb-3 lg:pb-0">
+          <Link to="/">
+            <img className="w-20 md:w-28 pb-2" src={logo} alt="footer logo" />
+          </Link>
           <p>
-          Providing exceptional legal and professional services with integrity and excellence. Tailored solutions to meet your unique needs. Contact us for expert advice and personalized support.
+            Providing exceptional legal and professional services with integrity
+            and excellence. Tailored solutions to meet your unique needs.
+            Contact us for expert advice and personalized support.
           </p>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 justify-center">
           <div className="md:flex justify-start items-center gap-4">
             <div class="flex items-center pt-3">
-              <img
-                src={emailImg}
-                alt="email icon"
-                class="w-6 h-auto mr-2"
-              />
+              <img src={emailImg} alt="email icon" class="w-6 h-auto mr-2" />
               <p>kmssassociates52@gmail.com</p>
             </div>
             <div class="flex items-center pt-3">
-              <img
-                src={callImg}
-                alt="call icon"
-                class="w-6 h-auto mr-2"
-              />
+              <img src={callImg} alt="call icon" class="w-6 h-auto mr-2" />
               <p>+880 18 93 97 25 09</p>
             </div>
           </div>
           <div class="flex items-center pt-3">
-            <img
-              src={locaionImg}
-              alt="location icon"
-              class="w-6 h-auto mr-2"
-            />
-            <p>
-            19/1, BL-K, Eastern Housing, Mirpur-12, Dhaka-1216
-            </p>
+            <img src={locaionImg} alt="location icon" class="w-6 h-auto mr-2" />
+            <p>19/1, BL-K, Eastern Housing, Mirpur-12, Dhaka-1216</p>
           </div>
         </div>
       </div>
       <hr />
       <div className="text-center md:flex items-center justify-between py-4 md:px-10 xl:px-3">
-        <p class="mt-4 text-base lg:text-lg">© {currentYear} All rights reserved by KMSS Associates.</p>
+        <p class="mt-4 text-base lg:text-lg">
+          © {currentYear} All rights reserved by KMSS Associates.
+        </p>
         {/* Social Icons */}
         <div className="flex justify-center items-center pt-3 gap-2">
           <a href="https://www.facebook.com">
