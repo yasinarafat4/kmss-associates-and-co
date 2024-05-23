5e18acd044367a5163345917fa96e1ad0aa742d2
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-05-23 21:23:29 +0600

    Proprietorship accordion added

--- src/pages/ServiceDetails/ServiceTwo.jsx
+++ src/pages/ServiceDetails/ServiceTwo.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import ServiceBanner from '../../components/ServiceBanner';
+import React, { useState } from "react";
+import ServiceBanner from "../../components/ServiceBanner";
 
 const ServiceTwo = () => {
-     // States
+  // States
   const [isOpen, setIsOpen] = useState(null);
   const toggle = (idx) => setIsOpen((prevIdx) => (prevIdx == idx ? null : idx));
 
-    return (
-        <div>
-             <ServiceBanner heading={'Our Entity Formation Service'} />
-              {/* Trade License */}
-        <div className=" rounded-lg border-2 border-blue-600 dark:border-white mb-4 lg:mb-8 bg-blue-100 dark:bg-slate-700 dark:text-slate-200">
+  return (
+    <>
+      <ServiceBanner heading={"Our Entity Formation Service"} />
+      <div className="px-[15px] md:px-[60px] xl:px-[120px] 2xl:px-[350px] py-[16px] lg:py-[40px] dark:bg-slate-900">
+        {/* Proprietorship */}
+        <div className="rounded-lg border-2 border-blue-600 dark:border-white mb-4 lg:mb-8 bg-blue-100 dark:bg-slate-700 dark:text-slate-200">
           <div>
             <button
               onClick={() => toggle(1)}
@@ -18,10 +19,10 @@ const ServiceTwo = () => {
             >
               <div className="space-y-3  text-start">
                 <h2 className="text-xl lg:text-2xl text-blue-600 dark:text-white font-semibold">
-                  1. Trade License
+                  1. Proprietorship
                 </h2>
                 <p className="text-md lg:text-lg text-blue-600 dark:text-white font-medium">
-                  Learn about our trade license.
+                Starting a Proprietorship in Bangladesh
                 </p>
               </div>
               <div className="rounded-full bg-white p-5">
@@ -62,29 +63,30 @@ const ServiceTwo = () => {
               <div className="overflow-hidden bg-white dark:bg-slate-800 p-4 md:py-4 lg:py-6 md:px-8 lg:px-12 text-blue-600 dark:text-white space-y-6">
                 {" "}
                 <p className="text-md lg:text-lg">
-                  KMSS & ASSOCIATES provides services in regard of application
-                  for Trade License local government / Pouroshava / Union/City
-                  Corporation in Bangladesh. Trade License is mandatory for
-                  every form of business entity in Bangladesh.
+                Generally three types of business operate in Bangladesh. These are Limited Company, Partnership Firm and Proprietorship Business. Among them, the most preferable trade is proprietorship business in Bangladesh. <br /> <br /> A single entrepreneur can start a proprietorship business. Generally, small and medium scale businesses are regularly forming by proprietorship as it is an easy way to form this type of business.
                 </p>
                 <p className="text-md lg:text-lg">
-                  It is issued by the local government of the respective areas.
-                  Every business entity must obtain Trade License from each
-                  local government where it operates. If a business entity has
-                  more than one place of business, it must obtain Trade License
-                  from each local government/Pouroshava/Union/City Corporation.
-                </p>
-                <p className="text-md lg:text-lg">
-                  It is issued for one year and have to be renewed annually.
-                  Trade License attracts some government fees, which usually
-                  depends on the types of business.
+                If someone wants to start proprietorship business at first make a Trade License in Bangladesh from local authority, he or she may apply to the City Corporation Office, Municipality Office, Upozila Council, Union Council or other respective Government authority.<br className="lg:hidden" /> <br className="lg:hidden" /> For having a Trade License firstly, one needs to select a name for his or her proprietorship business. <br /><br /> After that all the necessary documents have to be submitted and the necessary Govt.<br className="lg:hidden" /> <br className="lg:hidden" /> Fees have to be paid (depending on the kinds of business) to the Trade License issuing authority. After checking of all the papers, the Trade License is issued by the issuing body.
                 </p>
               </div>
             </div>
           </div>
         </div>
-        </div>
-    );
+      </div>
+
+
+      {/* i)	Proprietorship
+ii)	Partnership
+iii)	Private Limited
+iv)	Public Limited
+v)	Society / Trust / Foundation
+vi)	Trade Organization
+vii)	Foreign Company
+viii)	Joint Venture Company
+ix)	Liaison Or Branch Office */}
+
+    </>
+  );
 };
 
 export default ServiceTwo;
