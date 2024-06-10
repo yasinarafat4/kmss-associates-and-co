40a7b6038f29b3e0387c8c6ee715351b4bc14ab1
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-06-10 21:29:41 +0600

    Service banner changed

--- src/components/ServiceBanner.jsx
+++ src/components/ServiceBanner.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 const ServiceBanner = ({heading}) => {
     return (
-        <div className="flex items-center justify-center h-[200px] bg-[#0249AE] dark:bg-slate-700 text-white">
-            <div className="text-center">
-                <h2 className="text-3xl font-bold">{heading}</h2>
-                <p className="mt-2">Providing excellence in legal and professional consultancy.</p>
+        <div 
+            className="flex items-center justify-center h-[350px] bg-cover bg-center relative text-white"
+            style={{
+                backgroundImage: 'linear-gradient(rgba(2, 73, 174, 0.5), rgba(2, 73, 174, 0.5)), url("https://i.ibb.co/6NnPygz/2147656701-1.jpg")'
+            }}
+        >
+            <div className="text-center z-10">
+                <h2 className="text-4xl font-semibold">{heading}</h2>
+                <p className="mt-2 text-[18px]">Providing excellence in legal and professional consultancy.</p>
             </div>
+            <div className="absolute inset-0 bg-[#020508] opacity-50 dark:opacity-30"></div>
         </div>
     );
 };
