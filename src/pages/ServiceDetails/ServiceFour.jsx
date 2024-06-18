4412be6f4e18c1fd7fa03696ace01c1b86239c7a
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-06-19 00:34:40 +0600

    Tax Advisory Service added

--- src/pages/ServiceDetails/ServiceFour.jsx
+++ src/pages/ServiceDetails/ServiceFour.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import ServiceBanner from '../../components/ServiceBanner';
+import React from "react";
+import ServiceBanner from "../../components/ServiceBanner";
 
 const ServiceFour = () => {
-    return (
-        <div>
-            <ServiceBanner heading={'Our Tax Advisory Service'} />
-            
-        </div>
-    );
+  return (
+    <div>
+      <ServiceBanner heading={"Our Tax Advisory Service"} />
+      <div className="px-[20px] py-[10px] md:px-[100px] md:py-[30px] dark:bg-slate-700 dark:text-slate-200">
+        <p className="text-md lg:text-lg">
+          KMSS & Associates has a significant practice and experience in tax
+          services in Bangladesh. KMSS & Associates provides the best tax
+          lawyers in Bangladesh who are experts in tax law, which is generally
+          divided into two categories: Value Added Tax and Income Tax.
+        </p>
+        <br />
+        <p className="text-md lg:text-lg">
+          Additionally, there are other types of taxes like customs duty, gift
+          tax, supplementary duty, etc. With us, you get a complete solution for
+          your taxation matters.<br className="lg:hidden" /> <br className="lg:hidden" /> This includes advising on the tax implications
+          of various transactions an entity enters into, managing tax deducted
+          at source from salaries, and handling tax withheld from suppliers.<br className="lg:hidden" /> <br className="lg:hidden" /> We
+          also assist with the submission of quarterly withholding tax returns,
+          annual corporate tax returns, and the s. 108 return to the tax
+          authority.
+        </p>
+        <br />
+        <p className="text-md lg:text-lg">
+          We also represent clients before the taxation authority to settle any
+          disputes or represent clients in front of a tribunal or court of law
+          to settle or fight a tax appeal. It is very important that you receive
+          full value regarding tax-related services in Bangladesh. Our
+          consultants will ensure absolute compliance with finance and law.
+        </p>
+        <br />
+        <p className="text-md lg:text-lg">
+          We also provide regular services for personal income tax matters to
+          individuals, including preparation and submission of returns,
+          obtaining tax clearance certificates, and tax residency certificates.<br className="lg:hidden" /> <br className="lg:hidden" />
+          KMSS & Associates also provides major legal assistance for income tax
+          reference cases in Bangladesh in the Supreme Court of Bangladesh under
+          section 160 of the Income Tax Ordinance.
+        </p>
+        <br />
+        <p className="text-md lg:text-lg">
+          We are known for our integrity, efficiency, and a high degree of
+          partner involvement. Our reputation is founded on the simple
+          philosophy of personal, prompt service and advice relevant to the
+          modern requirements of business. <br className="lg:hidden" /> <br className="lg:hidden" />Your subscription to our service
+          would get you paired with our team of dedicated, highly vetted
+          professional accountants and tax consultants, who would learn your
+          business practices and processes and start providing the best
+          plausible solutions.
+        </p>
+        <br />
+        <p className="text-md lg:text-lg">
+          We maintain and uphold the highest quality standards for ourselves.
+          This is evident not only in our choice of partners and professional
+          staff but also in their further training.<br className="lg:hidden" /> <br className="lg:hidden" /> Our associates constantly
+          attend external continuing professional development seminars to
+          strengthen their specialized knowledge in various areas. Our
+          connections with universities, colleges, and professional associations
+          help us in pursuing this objective.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceFour;
