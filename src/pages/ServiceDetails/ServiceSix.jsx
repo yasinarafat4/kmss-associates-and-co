a3a41884b71e6736b8b619d22f8507d1534257ba
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-06-27 18:15:43 +0600

    Heading Added

--- src/pages/ServiceDetails/ServiceSix.jsx
+++ src/pages/ServiceDetails/ServiceSix.jsx
@@ -5,7 +5,10 @@ const ServiceSix = () => {
   return (
     <div>
       <ServiceBanner heading={"Our Stock Capital Market Service"} />
-      
+      <div className="px-[20px] py-[10px] md:px-[100px] md:py-[30px] dark:bg-slate-700 dark:text-slate-200">
+        <h2>Profit-Sharing Plans</h2>
+        
+      </div>
     </div>
   );
 };
