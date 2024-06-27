28f1e1f0f48545a9705f37d42a0f2e62d02fb5d9
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-06-27 18:23:00 +0600

    Class Added

--- src/pages/ServiceDetails/ServiceSix.jsx
+++ src/pages/ServiceDetails/ServiceSix.jsx
@@ -5,8 +5,8 @@ const ServiceSix = () => {
   return (
     <div>
       <ServiceBanner heading={"Our Stock Capital Market Service"} />
-      <div className="px-[20px] py-[10px] md:px-[100px] md:py-[30px] dark:bg-slate-700 dark:text-slate-200">
-        <h2>Profit-Sharing Plans</h2>
+      <div className="px-[20px] py-[10px] md:px-[100px] md:py-[20px] dark:bg-slate-700 dark:text-slate-200">
+        <h2 className="text-lg md:text-xl font-semibold mb-1">Profit-Sharing Plans</h2>
         
       </div>
     </div>
