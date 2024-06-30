3d92a247f597c795834329527a4ffb1a90cbeebe
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-06-30 22:52:53 +0600

    More Partners added

--- src/pages/Home/Partners.jsx
+++ src/pages/Home/Partners.jsx
@@ -1,17 +1,37 @@
 import React from "react";
-import logo1 from "../../assets/partners_logo/logo-1.png";
-import logo2 from "../../assets/partners_logo/logo-2.png";
-import logo3 from "../../assets/partners_logo/logo-3.png";
+import logo1 from "../../assets/partners_logo/logo-1.jpg";
+import logo2 from "../../assets/partners_logo/logo-2.jpg";
+import logo3 from "../../assets/partners_logo/logo-3.jpeg";
+import logo5 from "../../assets/partners_logo/logo-4.png";
+import logo4 from "../../assets/partners_logo/logo-5.png";
 
 const Partners = () => {
   const partners = [
     {
-      name: "ALKA Associates",
+      name: "Nahid Textile Mills",
       logo: logo1,
-      link: "https://alka-associates.com",
+      link: "https://nahidtextiles.com",
     },
-    { name: "Real Estate", logo: logo2, link: "https://www.realestate.com.au" },
-    { name: "UNEX", logo: logo3, link: "https://www.unex.com" },
+    {
+      name: "Adnan Textile Ltd.",
+      logo: logo2,
+      link: "https://www.facebook.com/adnantextiles",
+    },
+    {
+      name: "Disability Pride Bangladesh",
+      logo: logo3,
+      link: "https://www.linkedin.com/in/disability-pride-bangladesh-58a970286",
+    },
+    {
+      name: "Asian Imports Ltd.",
+      logo: logo4,
+      link: "https://asianimportsltd.com",
+    },
+    {
+      name: "Asian Holdings Ltd.",
+      logo: logo5,
+      link: "https://asianholdings.com.bd",
+    },
   ];
 
   return (
@@ -20,7 +40,7 @@ const Partners = () => {
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-6">
           Our Partners & Supporters
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
           {partners.map((partner, index) => (
             <a target="_blank" href={partner.link}>
               <div
@@ -30,11 +50,8 @@ const Partners = () => {
                 <img
                   src={partner.logo}
                   alt={partner.name}
-                  className="h-20 w-auto mb-2 dark:border-2 border-slate-300 dark:p-2"
+                  className="h-20 w-full md:w-auto mb-2 dark:border-2 border-slate-300 dark:p-2"
                 />
-                <span className="group-hover:text-[#0B539B] dark:group-hover:text-[#3cf0e7] duration-300 text-base text-slate-800 dark:text-slate-200">
-                  {partner.name}
-                </span>
               </div>
             </a>
           ))}
